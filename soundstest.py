ebab0b577645ef8e1a84a601802ccf4dddde6d11
Ivar Clemens <post@ivarclemens.nl>
2014-03-24 16:48:39 +0100

    troubleshotting

--- soundstest.py
+++ soundstest.py
@@ -2,13 +2,9 @@ import winsound
 import time
 
 time.sleep(20)
-winsound.Beep(2000,500)
-
-time.sleep(5)
-winsound.Beep(3000,500)
 
 time.sleep(5)
 winsound.Beep(4000,500)
 
-time.sleep(5)
-winsound.Beep(5000,500)
+
+winsound.Beep(4000,500)
